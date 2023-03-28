2a08946e6ce41a02440101cbab71b545f58f0b62
Janos <janosdescheemaeker@hotmail.com>
2023-03-28 13:46:31 +0200

    story 1 - added exception handling in controller

---
+++ src/main/java/com/eurder/api/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.eurder.api;
+
+import com.eurder.exception.InvalidInputException;
+import com.eurder.exception.MandatoryFieldException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(MandatoryFieldException.class)
+	private void mandatoryFieldException(MandatoryFieldException exception, HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+	}
+	@ExceptionHandler(InvalidInputException.class)
+	private void invalidInputException(InvalidInputException exception, HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+	}
+}
